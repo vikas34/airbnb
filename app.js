8a898de811b271c65214436da4b56feface09a75
Vikas <vky2334@gmail.com>
2025-06-26 00:22:41 +0530

    Complete CRUD

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const Listing = require("./models/listing.js");
-
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -19,18 +20,64 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
 }
 
-app.set("view engine", "ejs" );
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
 });
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Create Route  by Form
+app.post("/listings", async (req, res) => {
+  // let {title, description, image, price ,country, location} = req.body;
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect("/listings");
+});
+
+// Delete Route
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 // app.get("/testListing", async (req, res) => {
 //   const sampleListing = new Listing({
 //     title: "my Home",
--- models/listing.js
+++ models/listing.js
@@ -11,7 +11,7 @@ const listingSchema = new Schema({
   image: {
     url: {
       type: String,
-      required: true,
+      // required: true,
     },
   },
   price: Number,
